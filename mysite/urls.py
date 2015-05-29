946e553af20bce6f74b701bf9bafe9dd68000cf2
bearicc <hxiongustc@gmail.com>
2015-05-29 18:21:53 -0500

    .

--- mysite/urls.py
+++ mysite/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'mysite.views.home', name='home'),
     url(r'^blog/', include('blog.urls')),
+    url(r'^about$', 'mysite.views.about', name='about'),
 ]
