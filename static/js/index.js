8e4ae464d7f46dc1ef821236ab2f8bab832940d0
bearicc <hxiongustc@gmail.com>
2015-05-29 19:41:14 -0500

    .

---
+++ static/js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('.main-gallery').flickity({
+        cellAlign: 'center',
+        contain: true,
+        cellSelector: '.gallery-cell',
+        setGallerySize: false,
+        imagesLoaded: true,
+        wrapAround: true,
+        autoPlay: 2000,
+        pageDots: false
+    });
+});
