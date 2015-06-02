f0057745bc96684ac5d0f1f5c5318da4ecfae71a
bearicc <hxiongustc@gmail.com>
2015-06-02 18:19:52 -0500

    render resume.pdf

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from reportlab.pdfgen import canvas
 
 
 def home(request):
@@ -7,3 +9,15 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def resume(request):
+    with open('static/doc/resume.pdf', 'rb') as pdf:
+        response = HttpResponse(pdf.read(), content_type='application/pdf')
+        response['Content-Disposition'] = 'inline;filename="resume.pdf"'
+        # p = canvas.Canvas(response)
+        # p.drawString(100, 100, "Hello world.")
+        # p.showPage()
+        # p.save()
+        return response
+    pdf.closed
